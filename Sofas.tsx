e877f1908dcee9eb4ccb9a441b809fd4d6a45a82
Raphael Richter <richter@e-spirit.com>
2021-02-07 12:45:47 +0100

    Sofas

---
+++ Sofas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="Sofas" tilewidth="32" tileheight="32" tilecount="88" columns="8">
+ <image source="sofas_tileset.png" width="256" height="352"/>
+</tileset>
